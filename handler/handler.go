f3b74221f91013cb8313602bf8d8089ed57fd0e0
LeeJeKyun <leejk@metabuild.co.kr>
2025-02-25 17:41:31 +0900

    고루틴을 이용해 handler.go 개선

--- handler/handler.go
+++ handler/handler.go
@@ -5,7 +5,6 @@ import (
 	"go-mysql-crud/model"
 	"go-mysql-crud/store"
 	"io"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -20,66 +19,79 @@ func (c Client) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	case "GET":
 		//query에 key값이 없을 경우 모든 데이터 select
 		key := query.Get("key")
-		if key != "" {
-			keyNum, _ := strconv.Atoi(key)
-			result := store.GetDetail(keyNum)
-			json, err := json.Marshal(result)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			res.WriteHeader(200)
-			res.Write(json)
-		} else {
-			results := store.GetAll()
-			json, err := json.Marshal(results)
-			if err != nil {
-				log.Fatal(err)
-			}
-			res.Write(json)
-		}
-		//query에 key값이 있을 경우 해당 데이터만 Select
-	case "POST":
-		all, err := io.ReadAll(req.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		model := model.User{}
-		err = json.Unmarshal(all, &model)
-		if err != nil {
-			log.Fatal(err)
-		}
-		// 필요한 데이터가 전달될 경우 데이터 삽입
-		one := store.InsertOne(model)
-		result := strconv.FormatInt(one, 10)
-		res.Write([]byte(result))
+		//결과값 채널 생성
+		resultChan := make(chan any)
+		//에러 채널 생성
+		errChan := make(chan error)
 
+		//고루틴으로 key값에 따른 별도의 로직 실행(getAll, getOne)
+		go func() {
+			if key != "" {
+				keyNum, err := strconv.Atoi(key)
+				if err != nil {
+					errChan <- err
+					return
+				}
+				result := store.GetDetail(keyNum)
+				resultChan <- result
+			} else {
+				results := store.GetAll()
+				resultChan <- results
+			}
+		}()
+		//결과값에 따라 반환 혹은 에러 반환
+		select {
+		case result := <-resultChan:
+			jsonData, err := json.Marshal(result)
+			if err != nil {
+				http.Error(res, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			res.WriteHeader(http.StatusOK)
+			res.Write(jsonData)
+		case err := <-errChan:
+			http.Error(res, err.Error(), http.StatusBadRequest)
+		}
+	case "POST":
+		handleWriteRequest(res, req, store.InsertOne)
 	case "PUT":
-		// id를 제외한 다른 데이터를 수정 가능
-		all, err := io.ReadAll(req.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		model := model.User{}
-		err = json.Unmarshal(all, &model)
-		if err != nil {
-			log.Fatal(err)
-		}
-		one := store.UpdateOne(model)
-		result := strconv.FormatInt(one, 10)
-		res.Write([]byte(result))
+		handleWriteRequest(res, req, store.UpdateOne)
 	case "DELETE":
-		// id를 통해 데이터를 삭제 가능
-		all, err := io.ReadAll(req.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		model := model.User{}
-		err = json.Unmarshal(all, &model)
-		if err != nil {
-			log.Fatal(err)
-		}
-		one := store.DeleteOne(model)
-		result := strconv.FormatInt(one, 10)
-		res.Write([]byte(result))
+		handleWriteRequest(res, req, store.DeleteOne)
+	default:
+		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handleWriteRequest(res http.ResponseWriter, req *http.Request, dbFunc func(user model.User) int64) {
+	all, err := io.ReadAll(req.Body)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user := model.User{}
+	err = json.Unmarshal(all, &user)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resultChan := make(chan int64)
+	errChan := make(chan error)
+
+	go func() {
+		defer close(resultChan)
+		defer close(errChan)
+		id := dbFunc(user)
+		resultChan <- id
+	}()
+
+	select {
+	case id := <-resultChan:
+		res.WriteHeader(http.StatusOK)
+		res.Write([]byte(strconv.FormatInt(id, 10)))
+	case err := <-errChan:
+		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
