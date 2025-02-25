ddf753f78d690cd6cdca20ddd37f55f35c01198c
LeeJeKyun <leejk@metabuild.co.kr>
2025-02-25 16:27:02 +0900

    단순한 REST API 버전 1 완성, 버전 2는 고루틴을 활용하여 개선할 예정

--- server.go
+++ server.go
@@ -6,13 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-
 	c := new(handler.Client)
-
 	http.Handle("/client", c)
-
 	http.ListenAndServe(":8080", nil)
 }
